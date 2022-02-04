22e1aff9e027df4b7075344ba3f23eb92d83d8e5
horcaseb <46980001+horcaseb@users.noreply.github.com>
2022-02-04 10:43:10 -0500

    Add files via upload

---
+++ ExpansiveWave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpansiveWave : MonoBehaviour
+{
+    public float duration;
+    public AnimationCurve curve;
+    public Gradient grad;
+    public ParticleSystem particulas;
+
+    public ParticleSystem.ShapeModule shape;
+    public ParticleSystem.ColorOverLifetimeModule color;
+
+    public AudioSource source;
+    public Light luz;
+
+    private float t;
+    void Start()
+    {
+        particulas.Play();
+        source.Play();
+
+        shape = particulas.shape;
+        color = particulas.colorOverLifetime;
+
+        color.color = grad;
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        shape.radius = 5 * curve.Evaluate(t / duration);
+        source.volume = curve.Evaluate(t / duration);
+        luz.color = grad.Evaluate(t/duration);
+    }
+}
